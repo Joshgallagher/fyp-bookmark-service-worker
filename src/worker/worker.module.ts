2949a50404b829135010325a16971866ded7c034
Josh <joshgallagher24@gmail.com>
2020-05-18 21:32:10 +0100

    Setup RabbitMQ and included a sample env

--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
 import { WorkerService } from './worker.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
+  imports: [
+    ConfigModule,
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        exchanges: [
+          {
+            name: configService.get<string>('AMQP_EXCHANGE'),
+            type: 'topic',
+          },
+        ],
+        uri: configService.get<string>('AMQP_URI'),
+        connectionInitOptions: {
+          wait: false
+        },
+      }),
+      inject: [ConfigService]
+    }),
+  ],
   providers: [WorkerService]
 })
-export class WorkerModule {}
+export class WorkerModule { }
--- src/worker/worker.service.ts
+++ src/worker/worker.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 
 @Injectable()
-export class WorkerService {}
+export class WorkerService {
+    @RabbitSubscribe({
+        exchange: 'article.exchange',
+        routingKey: 'article.deleted',
+        queue: 'bookmark-service-queue'
+    })
+    async deleteRatingsHandler(
+        { id }: Record<string, number>
+    ): Promise<void> {
+    }
+}
