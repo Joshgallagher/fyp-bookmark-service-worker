4172d5d70660a960ce8bbf3d292f5b3ad50e1fa6
Josh <joshgallagher24@gmail.com>
2020-05-18 21:28:19 +0100

    Added core worker packages and setup config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { WorkerModule } from './worker/worker.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WorkerModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'test'
+        ? '.env.test'
+        : '.env'
+    }),
+    WorkerModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
